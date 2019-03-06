ec9c576b9658a1a659024773d8b1a1f0b2396296
ericanandagds <ericanandagds@gmail.com>
2019-03-06 13:09:52 -0300

    ok

---
+++ dist/out-tsc/src/app/pizza/pizza.page.js
@@ -0,0 +1,19 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+var PizzaPage = /** @class */ (function () {
+    function PizzaPage() {
+    }
+    PizzaPage.prototype.ngOnInit = function () {
+    };
+    PizzaPage = tslib_1.__decorate([
+        Component({
+            selector: 'app-pizza',
+            templateUrl: './pizza.page.html',
+            styleUrls: ['./pizza.page.scss'],
+        }),
+        tslib_1.__metadata("design:paramtypes", [])
+    ], PizzaPage);
+    return PizzaPage;
+}());
+export { PizzaPage };
+//# sourceMappingURL=pizza.page.js.map
