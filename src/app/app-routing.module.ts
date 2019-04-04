24e1d4c219a50c86a93a8318457c77bf96196476
ericanandagds <ericanandagds@gmail.com>
2019-04-04 15:25:37 -0300

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,9 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'cadastro', loadChildren: './cadastro/cadastro.module#CadastroPageModule' },  { path: 'tradicionais', loadChildren: './home/tradicionais/tradicionais.module#TradicionaisPageModule' },
-  { path: 'especiais', loadChildren: './home/especiais/especiais.module#EspeciaisPageModule' },
-  { path: 'doces', loadChildren: './home/doces/doces.module#DocesPageModule' },
-
+  { path: 'cadastro', loadChildren: './cadastro/cadastro.module#CadastroPageModule' },
+  {path: '' , loadChildren:'./home/home.module#HomeModule' },
   
-
-
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import {Routes, RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path : 'home',
+    component : HomePage,
+    children:[
+      {path:'', redirectTo:'/Home', pathMatch:'full'},
+      { path: 'tradicionais', loadChildren: './home/tradicionais/tradicionais.module#TradicionaisPageModule' },
+      { path: 'especiais', loadChildren: './home/especiais/especiais.module#EspeciaisPageModule' },
+      { path: 'doces', loadChildren: './home/doces/doces.module#DocesPageModule' },
+    ]
+  }
+
+
+]
+
+
+
+
+
+
+
+
+
 
 @NgModule({
   imports: [
