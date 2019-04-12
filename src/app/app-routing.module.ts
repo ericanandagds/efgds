7921490b13c55d69571b5d3763ba34a24a6afb6c
ericanandagds <ericanandagds@gmail.com>
2019-04-12 15:39:15 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AutenticacaoGuard} from './guard/autenticacao.guard';
 
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' , canActivate:[AutenticacaoGuard]},
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'cadastro', loadChildren: './cadastro/cadastro.module#CadastroPageModule' },
-  { path: '', loadChildren: './home/home.module#HomePageModule'},  { path: 'pagamento', loadChildren: './pagamento/pagamento.module#PagamentoPageModule' },
+  { path: '', loadChildren: './home/home.module#HomePageModule'},
+  { path: 'pagamento', loadChildren: './pagamento/pagamento.module#PagamentoPageModule' },
   { path: 'bebidas', loadChildren: './bebidas/bebidas.module#BebidasPageModule' },
 
   
