89adbc49eee44639b169367f4b863dc7dd9cd632
ericanandagds <ericanandagds@gmail.com>
2019-06-12 18:12:05 -0300

    12/06

--- src/app/services/pizza.service.ts
+++ src/app/services/pizza.service.ts
@@ -6,27 +6,31 @@ import { pizza } from '../../../models/pizza';
   providedIn: 'root'
 })
 export class PizzaService {
+  [x: string]: any;
 
   private db: firebase.firestore.CollectionReference;
   private userID;
+  
   constructor(){
     let userID = firebase.auth().currentUser.uid;
     this.db = firebase.firestore().collection('pizza');
   }
+  
   cadastrar(pizza: pizza) {
-    this.db.ref('pizza').once('value').then (snapshot =>{
-      snapshot.forEach(pizza=>{
-        console.log(pizza.val());
-      });
-    });
+    let doc = this.db.doc();
+    pizza.id = doc.id;
+    pizza.pizzaID = this.userID;
+    let obj = this.castObject(pizza);
+    doc.set(obj);
    }
+
    editar(pizza: pizza) {
     this.db.doc(pizza.id).set(pizza);
   }
   excluir (id: string) {
     this.db.doc(id).delete();
   }
-     
+
   async buscarTodos(): Promise<pizza[]> {
     return this.db.where('pizzaID', '==', this.userID).get().then(snapshot => {
       let pizza = [];
